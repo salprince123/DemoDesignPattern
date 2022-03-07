0f2ae8504b7a6220057a25468a0e56f25ee8e1b1
Dieu Le <18520265@gm.uit.edu.vn>
2022-03-07 15:02:20 +0700

    init commit

---
+++ Adapter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTrack staticTrack = new MyTrack();
+            Console.Write("TRAIN :");
+            MyTrain train = new MyTrain();            
+            staticTrack.Active(train);
+            //sau do nguoi ta them xe bus 
+            MyCar car = new MyCar();
+            //nguoi ta muon xe bus chay tren duong ray 
+            //staticTrack.Active(bus);
+            //khoong khop nen khong xai duoc
+            //=> khong thay doi duoc track va train ==> cai them adapter
+            Console.Write("CAR :");
+            MyTrainAdapter adapter= new MyTrainAdapter(car);
+            staticTrack.Active(adapter);
+        }
+    }
+
+    internal class MyTrainAdapter : MyTrain
+    {
+        private MyCar _car;
+        public MyTrainAdapter(MyCar car)
+        {
+            _car = car;
+        }
+    }
+
+    internal class MyCar
+    {
+        public MyCar()
+        {
+        }
+    }
+
+    internal class MyTrain
+    {
+        public MyTrain()
+        {
+        }
+    }
+
+    internal class MyTrack
+    {        
+        public void Active(MyTrain train)
+        {
+            Console.WriteLine("This vehicle can run on a track !");
+        }
+    }
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bakery store = new Bakery();
+            Console.Write("BEFORE DECORATE: ");
+            Cake simpleCake = new Cake();
+            store.MakeCake(new Cake());
+            // trang tri them cho banh 
+            Console.Write("AFTER DECORATE CREAM: ");
+            CreamCake myCake = new CreamCake(simpleCake);
+            store.MakeCake(myCake);
+            Console.Write("AFTER DECORATE CHOCOLATE: ");
+            ChocolateCake finalCake = new ChocolateCake(myCake);
+            store.MakeCake(finalCake);
+        }
+    }
+    public abstract class Product
+    {
+        public abstract string Make();
+    }
+    public class Cake: Product
+    {
+        public override string Make()
+        {
+            return "Cake";
+        }
+    }
+    public abstract class CakeDecorator : Product
+    {
+        protected Product _product;
+
+        public CakeDecorator(Product product)
+        {
+            this._product = product;
+        }
+
+        public void SetComponent(Product product)
+        {
+            this._product = product;
+        }
+        public override string Make()
+        {
+            if (this._product != null)
+            {
+                return this._product.Make();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+    public class CreamCake : CakeDecorator
+    {
+        public CreamCake(Product product) : base(product)
+        {
+        }
+        public override string Make()
+        {
+            return $"Cream {base.Make()}";
+        }
+    }
+    public class ChocolateCake : CakeDecorator
+    {
+        public ChocolateCake(Product product) : base(product)
+        {
+        }
+
+        public override string Make()
+        {
+            return $"Chocolate {base.Make()}";
+        }
+    }
+    internal class Bakery
+    {
+        public Bakery()
+        {
+        }
+        public void MakeCake(Product product)
+        {
+            Console.WriteLine("You have a " + product.Make());
+        }
+    }
+
+    
+}
